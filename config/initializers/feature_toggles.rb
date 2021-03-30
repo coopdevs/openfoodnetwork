bf04646fd2d27312fba3b1d9597aeadd404482d7
Pau Perez <saulopefa@gmail.com>
2021-03-30 20:03:01 +0200

    Enable :customer_balance toggle in testing

--- config/initializers/feature_toggles.rb
+++ config/initializers/feature_toggles.rb
@@ -3,6 +3,8 @@ require 'open_food_network/feature_toggle'
 beta_testers = ENV['BETA_TESTERS']&.split(/[\s,]+/) || []
 
 OpenFoodNetwork::FeatureToggle.enable(:customer_balance) do |user|
+  next true if Rails.env.test?
+
   if beta_testers == ['all']
     true
   else
